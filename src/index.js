6310c9cf3ddc7346a6101f2086836bb11a80a5cd
newming <a977527479@sina.com>
2016-08-11 10:29:06 +0800

    deploy

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import App from './App.js';
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
-render(<App />,document.getElementById('root'));
